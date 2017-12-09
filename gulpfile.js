e0a1ccb6e7d66ffbfd8414e49e0fa181c343be24
Sergey Olizarevich <neuxunil@yandex.ru>
2017-12-09 21:44:42 +0300

    Сделана сборка с gulp и webpack

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug'
+    },
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'build/assets/css/'
+    },
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'build/assets/js/'
+    }
+}
+
+// PUG
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(paths.root));
+}
+
+// SCSS
+function styles() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+// Удаление папки  dist
+function clean() {
+    return del(paths.root);
+}
+
+// WEBPACk
+function scripts() {
+    return gulp.src('src/scripts/*.js')
+        .pipe(gulpWebpack(webpackConfig, webpack))
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Слежение за файлами
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+// Локальный сервер + Livereload
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+// Перекладывание изображений
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+exports.images = images;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images, scripts),
+    gulp.parallel(watch, server)
+));
